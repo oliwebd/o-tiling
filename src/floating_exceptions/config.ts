bf9623fdfb8c14fd638f75dd2983e2aec4762e67
Ben Leppke <benleppke@gmail.com>
2026-07-25 10:37:58 -0500

    fix: float wl-clipboard windows to prevent tiling flash

--- src/floating_exceptions/config.ts
+++ src/floating_exceptions/config.ts
@@ -59,6 +59,7 @@ export const DEFAULT_FLOAT_RULES: Array<FloatRule> = [
     { class: 'zoom' },
     { class: '^.*action=join.*$' },
     { class: 'gjs' },
+    { class: 'io.github.bugaevc.wl-clipboard' },
 ];
 
 export interface WindowRule {
